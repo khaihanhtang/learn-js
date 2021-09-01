3dd2e6c44f0e6c88cccb849576231619bb663f48
khaihanhtang <84257457+khaihanhtang@users.noreply.github.com>
2021-09-02 00:07:18 +0800

    Update

--- youtube-reader/src/App.js
+++ youtube-reader/src/App.js
@@ -1,24 +1,45 @@
+import _ from 'lodash';
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/search_bar';
-import API_KEY from './data/constants';
+import {API_KEY} from './data/constants';
 import YTSearch from 'youtube-api-search';
 import React, { Component } from 'react';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {videos : []};
-    YTSearch({key : API_KEY, term : 'surfboard'}, data => {
-      this.setState({videos : data});
+    this.state = {
+      videos : [],
+      selectedVideo : null
+    };
+    this.videoSearch('surfboard');
+  }
+
+  videoSearch(term) {
+    YTSearch({key : API_KEY, term : term}, data => {
+      this.setState(
+        {
+          videos : data,
+          selectedVideo: data[0]
+        }
+      );
+      console.log(data);
     });
   }
+
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return (
       <div className="App">
-        <SearchBar />
-        <VideoList videos={this.state.videos}/>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          onVideoSelect={selection => this.setState({videos : this.state.videos, selectedVideo : selection})}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
